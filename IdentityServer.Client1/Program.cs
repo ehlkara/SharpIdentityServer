51e7a67bcc0c1cd5d60ced47e76c28dd6e1aed2b
ehlkara <ehlullah.fb@gmail.com>
2023-06-17 19:50:22 +0300

    take access token

--- IdentityServer.Client1/Program.cs
+++ IdentityServer.Client1/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddAuthentication(options =>
     opts.ClientSecret = "secret";
     opts.ResponseType = "code id_token";
     opts.GetClaimsFromUserInfoEndpoint = true;
+    opts.SaveTokens = true;
 });
 
 
