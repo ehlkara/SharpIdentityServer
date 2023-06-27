2a5f87d5c5f451204bb135762ca606973facdc92
ehlkara <ehlullah.fb@gmail.com>
2023-06-27 14:27:05 +0300

    created login form

---
+++ IdentityServer.Client1/Views/Login/Index.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityServer.Client1.Views.Login
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
