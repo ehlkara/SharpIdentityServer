7848a606370e1a352f5352dee71a26e709077286
ehlkara <ehlullah.fb@gmail.com>
2023-06-27 15:29:00 +0300

    created login method

--- IdentityServer.Client1/Controllers/LoginController.cs
+++ IdentityServer.Client1/Controllers/LoginController.cs
@@ -1,16 +1,89 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using IdentityModel.Client;
+using IdentityServer.Client1.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace IdentityServer.Client1.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginViewModel loginViewModel)
+        {
+            var client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync(_configuration["AuthServerUrl"]);
+
+            if(disco.IsError)
+            {
+                // catch error and logging
+            }
+
+            var password = new PasswordTokenRequest();
+
+            password.Address = disco.TokenEndpoint;
+            password.UserName = loginViewModel.Email;
+            password.Password = loginViewModel.Password;
+            password.ClientId = _configuration["Client-ResourceOwner:ClientId"];
+            password.ClientSecret = _configuration["Client-ResourceOwner:ClientSecret"];
+
+            var token = await client.RequestPasswordTokenAsync(password);
+
+            if(token.IsError)
+            {
+                // catch error and logging
+            }
+
+            var userInfoRequest = new UserInfoRequest();
+
+            userInfoRequest.Token = token.AccessToken;
+            userInfoRequest.Address = disco.UserInfoEndpoint;
+
+            var userInfo = await client.GetUserInfoAsync(userInfoRequest);
+
+            if(userInfo.IsError)
+            {
+                // catch error and logging
+            }
+
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(userInfo.Claims,CookieAuthenticationDefaults.AuthenticationScheme,
+                "name","role");
+
+            ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+            var authenticationProperties = new AuthenticationProperties();
+
+            authenticationProperties.StoreTokens(new List<AuthenticationToken>()
+            {
+                new AuthenticationToken {Name = OpenIdConnectParameterNames.AccessToken, Value = token.AccessToken},
+                new AuthenticationToken {Name = OpenIdConnectParameterNames.RefreshToken, Value = token.RefreshToken},
+                new AuthenticationToken {Name = OpenIdConnectParameterNames.ExpiresIn, Value =
+                DateTime.UtcNow.AddSeconds(token.ExpiresIn).ToString("O",CultureInfo.InvariantCulture)}
+            });
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal, authenticationProperties);
+
+            return RedirectToAction("Index", "User");
         }
     }
 }
